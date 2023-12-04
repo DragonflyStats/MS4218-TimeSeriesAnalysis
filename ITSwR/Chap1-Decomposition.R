0125df3d3f58b8ca787d604be2e9f8c132a5447b
DragonflyStats <kobriendublin@gmail.com>
2023-12-04 10:48:19 +0000

    Delete ITSwR/Chap1-Decomposition.R

--- ITSwR/Chap1-Decomposition.R
+++
@@ -1,8 +0,0 @@
-#Section 1.5.5 Decomposition in R
-
-plot(decompose(Elec.ts))
-Elec.decom <- decompose(Elec.ts , type=”mult”)
-plot(Elec.decom)
-Trend <- Elec.decom$trend
-Seasonal <- Elec.decom$seasonal
-ts.plot(cbind(Trend, Trend * Seasonal),lty=1:2)
